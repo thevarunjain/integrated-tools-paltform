3053868538295aaf42e4dcfdad7bdd16e8f84255
Abhishek Konduri <abhishek.konduri@sjsu.edu>
2020-04-28 17:27:38 -0700

    NoOfIssue GitHub Added

--- GitHub/src/main/java/com/IntegratedTools/GitHub/Issues/NoOfIssues.java
+++ GitHub/src/main/java/com/IntegratedTools/GitHub/Issues/NoOfIssues.java
@@ -4,10 +4,7 @@ import com.IntegratedTools.GitHub.Constants.Constants;
 import com.IntegratedTools.GitHub.HttpConnectionAndResponseHandler.HttpConnectionAndResponseHandler;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -15,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/integratedTools/github")
 public class NoOfIssues extends Constants {
